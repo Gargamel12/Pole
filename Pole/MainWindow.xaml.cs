b78cfc279c6e512a3584fabb44a127d15bd23b05
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2022-02-15 09:34:21 +0100

    H

--- Pole/MainWindow.xaml.cs
+++ Pole/MainWindow.xaml.cs
@@ -20,9 +20,10 @@ namespace Pole
     /// </summary>
     public partial class MainWindow : Window
     {
-        //int a;
-        //int b;
+        int a; //int a;
+        int b; //int b;
         int[] pole = new int[100];
+        List<int> seznam = new List<int>();
         public MainWindow()
         {
             InitializeComponent();
@@ -33,8 +34,7 @@ namespace Pole
             Random random = new Random();
             //a = random.Next(1,100);
             //b =  random.Next(1,100);
-            a = random.Next(1, 100);
-            b = random.Next(1, 100);
+           
             for (int i = 0; i < pole.Length; i++)
             {
                 txtvypis.Text += pole[i].ToString() + ";";
@@ -43,6 +43,25 @@ namespace Pole
             txtvypis.Text += string.Join(";", pole);
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            seznam.Add(random.Next(1, 100));
 
+            txtvypis.Text = "";
+            for (int i = 0; i < seznam.Count; i++)
+            {
+                txtvypis.Text += seznam[i].ToString() + ";";
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (seznam.Count > 0)
+            {
+                seznam.RemoveAt(0);
+            }
+            txtvypis.Text = string.Join(";", pole);
+        }
     }
 }
